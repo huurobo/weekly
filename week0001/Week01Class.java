a6b51da37da43ec08ab0dff9abc5496e6ce420ba
Hữu Robo <kikiem02@gmail.com>
2016-09-30 15:14:03 +0700

    Java
    Week01

---
+++ week0001/Week01Class.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week01;
+/**
+ *
+ * @author HuuRobo-PC
+ */
+public class Week01Class {
+    public static void main(String[] args){
+        for(int i=1; i<=100; i++){
+            if(i%3 == 0){
+                System.out.println("Fizz");
+            }
+            if(i%5 == 0){
+                System.out.println("Buzz");
+            }
+        }
+    }
+}
